6f81b68ca7f46d613af481580397812dac12af07
karthika2515 <karthika7june@gmail.com>
2021-02-02 08:29:15 +0530

    demo1 edited

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -2,7 +2,7 @@
 public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Push to Remote");
-		System.out.println("Push to local");
+		System.out.println("Push from local to remote");
 	}
 
 }
